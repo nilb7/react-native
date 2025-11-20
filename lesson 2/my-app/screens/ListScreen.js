d9dd8bbd2b811421a88e4c5e77595c394222b85e
nilb7 <tieenn298@gmail.com>
2025-11-20 20:36:13 +0100

    ss

--- lesson 2/my-app/screens/ListScreen.js
+++ lesson 2/my-app/screens/ListScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, FlatList, Button,  } from "react-native";
+import { Text, StyleSheet, View, FlatList, Button, TouchableOpacity } from "react-native";
 
 const students = [
   { name: "Eden", surname: "Rocha", age: "17" },
